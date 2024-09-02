23acaad8aa2b3c9028b98e0bc25da8de25705024
KwonJiYong <918513194@qq.com>
2024-09-02 15:04:51 +0800

    feat: 增加Redis自动配置

--- thinker-cloud-components/thinker-cloud-redis-starter/src/main/java/com/thinker/cloud/redis/config/RedisAutoConfiguration.java
+++ thinker-cloud-components/thinker-cloud-redis-starter/src/main/java/com/thinker/cloud/redis/config/RedisAutoConfiguration.java
@@ -9,13 +9,14 @@ import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
 import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizers;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.connection.RedisClusterConfiguration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.*;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
@@ -23,16 +24,19 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.util.CollectionUtils;
 
-import java.util.HashMap;
+import java.time.Duration;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.Objects;
 
 /**
  * redis 自动配置类
  *
  * @author admin
  **/
+@EnableCaching
 @Configuration
 @AllArgsConstructor
 @AutoConfigureBefore(RedisAutoConfiguration.class)
@@ -40,32 +44,56 @@ public class RedisAutoConfiguration {
 
     private final RedisProperties redisProperties;
 
-
     @Bean
     @Primary
-    public GenericObjectPoolConfig<?> redisPoolConfig() {
-        return new GenericObjectPoolConfig<>();
+    public LettuceClientConfiguration lettuceClientConfiguration() {
+        // Redis连接池配置
+        RedisProperties.Pool redisPool = redisProperties.getLettuce().getPool();
+        GenericObjectPoolConfig<Object> redisPoolConfig = new GenericObjectPoolConfig<>();
+        redisPoolConfig.setMinIdle(redisPool.getMinIdle());
+        redisPoolConfig.setMaxIdle(redisPool.getMaxIdle());
+        redisPoolConfig.setMaxWait(redisPool.getMaxWait());
+        redisPoolConfig.setMaxTotal(redisPool.getMaxActive());
+        redisPoolConfig.setTimeBetweenEvictionRuns(Duration.ofMillis(100));
+        return LettucePoolingClientConfiguration.builder().poolConfig(redisPoolConfig).build();
     }
 
     @Bean
     @Primary
-    public RedisClusterConfiguration redisClusterConfiguration() {
-        Map<String, Object> source = new HashMap<>(8);
-        List<String> nodes = redisProperties.getCluster().getNodes();
-        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration(nodes);
-        redisClusterConfiguration.setPassword(redisProperties.getPassword());
-        return redisClusterConfiguration;
+    @ConditionalOnBean(LettuceClientConfiguration.class)
+    public RedisConnectionFactory redisConnectionFactory(LettuceClientConfiguration lettuceClientConfig) {
+        // 集群模式
+        RedisProperties.Cluster cluster = redisProperties.getCluster();
+        if (Objects.nonNull(cluster) && !CollectionUtils.isEmpty(cluster.getNodes())) {
+            RedisClusterConfiguration redisClusterConfig = new RedisClusterConfiguration(cluster.getNodes());
+            redisClusterConfig.setUsername(redisProperties.getUsername());
+            redisClusterConfig.setMaxRedirects(cluster.getMaxRedirects());
+            redisClusterConfig.setPassword(RedisPassword.of(redisProperties.getPassword()));
+            return new LettuceConnectionFactory(redisClusterConfig, lettuceClientConfig);
+        }
+
+        // 哨兵模式
+        RedisProperties.Sentinel sentinel = redisProperties.getSentinel();
+        if (Objects.nonNull(sentinel) && !CollectionUtils.isEmpty(sentinel.getNodes())) {
+            RedisSentinelConfiguration redisSentinelConfig = new RedisSentinelConfiguration(
+                    sentinel.getMaster(), new HashSet<>(sentinel.getNodes()));
+            redisSentinelConfig.setUsername(sentinel.getUsername());
+            redisSentinelConfig.setDatabase(redisProperties.getDatabase());
+            redisSentinelConfig.setPassword(RedisPassword.of(redisProperties.getPassword()));
+            return new LettuceConnectionFactory(redisSentinelConfig, lettuceClientConfig);
+        }
+
+        // 单机模式
+        RedisStandaloneConfiguration redisStandaloneConfig = new RedisStandaloneConfiguration();
+        redisStandaloneConfig.setDatabase(redisProperties.getDatabase());
+        redisStandaloneConfig.setHostName(redisProperties.getHost());
+        redisStandaloneConfig.setPort(redisProperties.getPort());
+        redisStandaloneConfig.setPassword(RedisPassword.of(redisProperties.getPassword()));
+        return new LettuceConnectionFactory(redisStandaloneConfig, lettuceClientConfig);
     }
 
     @Bean
     @Primary
-    public LettuceConnectionFactory lettuceConnectionFactory(GenericObjectPoolConfig<?> redisPoolConfig,
-                                                             RedisClusterConfiguration redisClusterConfig) {
-        LettuceClientConfiguration clientConfiguration = LettucePoolingClientConfiguration.builder().poolConfig(redisPoolConfig).build();
-        return new LettuceConnectionFactory(redisClusterConfig, clientConfiguration);
-    }
-
-    @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         // 1.创建RedisTemplate对象
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
@@ -92,6 +120,7 @@ public class RedisAutoConfiguration {
     }
 
     @Bean
+    @Primary
     public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
         // 1.创建RedisTemplate对象
         StringRedisTemplate redisTemplate = new StringRedisTemplate();
@@ -105,15 +134,13 @@ public class RedisAutoConfiguration {
         redisTemplate.setHashKeySerializer(stringRedisSerializer);
 
         // 4.配置Value序列化
-        StringRedisSerializer stringRedisSerializer1 = new StringRedisSerializer();
-        redisTemplate.setValueSerializer(stringRedisSerializer1);
-        redisTemplate.setHashValueSerializer(stringRedisSerializer1);
+        redisTemplate.setValueSerializer(stringRedisSerializer);
+        redisTemplate.setHashValueSerializer(stringRedisSerializer);
 
         // 5.初始化StringRedisTemplate
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
     }
-
 
     @Bean
     @ConditionalOnMissingBean(CacheManagerCustomizers.class)
