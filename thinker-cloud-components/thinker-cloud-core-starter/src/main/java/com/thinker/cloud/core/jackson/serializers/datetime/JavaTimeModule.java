4dd50f9492280333ef09214da9b6176f1ebc2666
KwonJiYong <918513194@qq.com>
2024-09-05 17:01:30 +0800

    feat: 优化代码

--- thinker-cloud-components/thinker-cloud-core-starter/src/main/java/com/thinker/cloud/core/jackson/serializers/datetime/JavaTimeModule.java
+++ thinker-cloud-components/thinker-cloud-core-starter/src/main/java/com/thinker/cloud/core/jackson/serializers/datetime/JavaTimeModule.java
@@ -1,8 +1,12 @@
 package com.thinker.cloud.core.jackson.serializers.datetime;
 
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.PackageVersion;
+import com.fasterxml.jackson.datatype.jsr310.deser.InstantDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.InstantSerializer;
 
 import java.io.Serial;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -18,6 +22,8 @@ public class JavaTimeModule extends SimpleModule {
     private static final long serialVersionUID = -9029872512752022050L;
 
     public JavaTimeModule() {
+        super(PackageVersion.VERSION);
+
         // ======================= 时间序列化规则 ===============================
         // yyyy-MM-dd HH:mm:ss
         this.addSerializer(LocalDateTime.class, LocalDateTimeSerializer.INSTANCE);
@@ -25,6 +31,8 @@ public class JavaTimeModule extends SimpleModule {
         this.addSerializer(LocalDate.class, LocalDateSerializer.INSTANCE);
         // HH:mm:ss
         this.addSerializer(LocalTime.class, LocalTimeSerializer.INSTANCE);
+        // Instant 类型序列化
+        this.addSerializer(Instant.class, InstantSerializer.INSTANCE);
 
         // ======================= 时间反序列化规则 ==============================
         // yyyy-MM-dd HH:mm:ss
@@ -33,6 +41,8 @@ public class JavaTimeModule extends SimpleModule {
         this.addDeserializer(LocalDate.class, LocalDateDeserializer.INSTANCE);
         // HH:mm:ss
         this.addDeserializer(LocalTime.class, LocalTimeDeserializer.INSTANCE);
+        // Instant 反序列化
+        this.addDeserializer(Instant.class, InstantDeserializer.INSTANT);
     }
 
 }
